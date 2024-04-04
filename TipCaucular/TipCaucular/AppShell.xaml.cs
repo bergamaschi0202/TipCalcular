69702ec2c0edc2ecdb4b6d8eb4caa275957df216
Arthur <bergamaschiarthur6@gmail.com>
2024-04-04 12:52:52 -0300

    feat: Adicionei o projeto

---
+++ TipCaucular/TipCaucular/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace TipCaucular
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ TipCaucular/TipCaucular/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿namespace TipCaucular
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Percentage15_Clicked(object sender, EventArgs e)
+        {
+            TipPercentage.Value = 15;
+        }
+
+        private void Percentage30_Clicked(object sender, EventArgs e)
+        {
+            TipPercentage.Value = 30;
+        }
+
+        private void PercentageRoundDown_Clicked(object sender, EventArgs e)
+        {
+            double gorgeta = TipPercentage.Value;
+        }
+
+        private void PercentageRoundUp_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void OnTipPercentage_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            double valorDoSlider = TipPercentage.Value;
+            TipPercentage.Text = valorDoSlider.ToString("2");
+        }
+    }
+
+}
